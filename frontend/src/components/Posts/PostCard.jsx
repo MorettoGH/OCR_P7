de947fbdf2a684c74a0c3dbe57fc627d6f952df0
MorettoGH <christophe.moretto@gmail.com>
2022-07-28 17:26:27 +0200

    dynamic wall done (miss timestamp, email & like system)

--- frontend/src/components/Posts/PostCard.jsx
+++ frontend/src/components/Posts/PostCard.jsx
@@ -1,53 +1,38 @@
 import './PostCard.css';
-import Img from "../../data/profile.png"
+import axios from 'axios';
+import {useState} from 'react'
+import { useEffect } from 'react';
 
 function PostCard() {
+    const [postList, setPostList] = useState([]);
+    const auth = localStorage.getItem("token");
+    useEffect(() => {
+        axios({
+            method: 'GET',
+            url: 'http://localhost:4200/api/wall/',
+            headers: {'Authorization': 'Bearer ' + auth}
+            })
+            .then(res => {
+                setPostList(res.data);
+                console.log(res.data);
+            })
+    }, []);
+
     return (
         <div className='gm-postcard-container'>
-            <div className='gm-postcard'>
+            {postList.map((post) => (
+                <div key={post._id} className='gm-postcard'>
                 <div>
-                    <img src={Img} alt="" className='gm-postcard-img'></img>
+                    <img src={post.imageUrl} alt="" className='gm-postcard-img'></img>
                     <p className='gm-postcard-like'>Like</p>
                 </div>
                 <div className='gm-postcard-content'>    
                     <h3 className='gm-postcard-title'>toto@gmail.com</h3>
-                    <p className='gm-postcard-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor blandit enim. Aliquam placerat tempor vehicula. Suspendisse eleifend urna nec ligula commodo dapibus. Ut faucibus mauris a augue consequat finibus. Donec tempus arcu id eros interdum, sed elementum velit placerat. In sed augue risus.</p>
+                    <p className='gm-postcard-text'>{post.content}</p>
                     <p className='gm-postcard-date'> Publié le : 01/01/2023 at 10am</p>
                 </div>
             </div>
-            <div className='gm-postcard'>
-                <div>
-                    <img src={Img} alt="" className='gm-postcard-img'></img>
-                    <p className='gm-postcard-like'>Like</p>
-                </div>
-                <div className='gm-postcard-content'>    
-                    <h3 className='gm-postcard-title'>toto@gmail.com</h3>
-                    <p className='gm-postcard-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor blandit enim. Aliquam placerat tempor vehicula. Suspendisse eleifend urna nec ligula commodo dapibus. Ut faucibus mauris a augue consequat finibus. Donec tempus arcu id eros interdum, sed elementum velit placerat. In sed augue risus. Pellentesque vel urna nunc. Nam a sapien odio. Nulla vel tincidunt neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor blandit enim. Aliquam placerat tempor vehicula. Suspendisse eleifend urna nec ligula commodo dapibus. Ut faucibus mauris a augue consequat finibus. Donec tempus arcu id eros interdum, sed elementum velit placerat. In sed augue risus. Pellentesque vel urna nunc. Nam a sapien odio. Nulla vel tincidunt neque.</p>
-                    <p className='gm-postcard-date'> Publié le : 01/01/2023 at 10am</p>
-                </div>
-            </div>
-            <div className='gm-postcard'>
-                <div>
-                    <img src={Img} alt="" className='gm-postcard-img'></img>
-                    <p className='gm-postcard-like'>Like</p>
-                </div>
-                <div className='gm-postcard-content'>    
-                    <h3 className='gm-postcard-title'>toto@gmail.com</h3>
-                    <p className='gm-postcard-text'>Blablabla</p>
-                    <p className='gm-postcard-date'> Publié le : 01/01/2023 at 10am</p>
-                </div>
-            </div>
-            <div className='gm-postcard'>
-                <div>
-                    <img src={Img} alt="" className='gm-postcard-img'></img>
-                    <p className='gm-postcard-like'>Like</p>
-                </div>
-                <div className='gm-postcard-content'>    
-                    <h3 className='gm-postcard-title'>toto@gmail.com</h3>
-                    <p className='gm-postcard-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor blandit enim. Aliquam placerat tempor vehicula.</p>
-                    <p className='gm-postcard-date'> Publié le : 01/01/2023 at 10am</p>
-                </div>
-            </div>
+            ))}
         </div>
     )
 }
--- frontend/src/pages/Wall.jsx
+++ frontend/src/pages/Wall.jsx
@@ -1,26 +1,15 @@
 import PagesHeader from "../components/Headers/PagesHeader";
 import NewPostBtn from "../components/Buttons/NewPostBtn";
 import PostCard from "../components/Posts/PostCard";
-//import {useState, useEffect} from 'react';
 
 function Wall() {
-  /*const [authenticated, setauthenticated] = useState(null);
-  useEffect(() => {
-    const auth = localStorage.getItem("token");
-    if (auth) {
-      setauthenticated(auth);
-    }}, []);
-    if (!authenticated) {
-      //redirect
-    }else{*/
-      return (
-        <div>
-          <PagesHeader />
-          <NewPostBtn />
-          <PostCard />
-        </div>
-        )
-    }
-//}
+    return (
+      <div>
+        <PagesHeader />
+        <NewPostBtn />
+        <PostCard />
+      </div>
+      )
+}
 
 export default Wall;
